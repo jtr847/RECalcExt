b4593685c7aae01bf64527f54dd7cade4ae1f7bd
jtr847 <jtr847@gmail.com>
2013-12-21 13:07:24 -0500

    CheckBoxes
    Added CheckBoxes to select either fha loan or traditional loan.

--- Calcs/houseInfo.js
+++ Calcs/houseInfo.js
@@ -1,18 +1,14 @@
-var houseInfo = function (price)
-{
-// var houseProps = {cost: price, fha:{}}
-// houseProps.downPaymentTrad = price * 0.2;
-// // houseProps.downPaymentFha = price * 0.035;
-// houseProps.fha.downPayment = price * 0.035;
-tradDownPayment(price)
-fhaDownPayment(price)
-// return houseProps.downPaymentTrad;
-};
+var houseInfo = function (price){
+	var trad = document.getElementsByName('loan')[0].checked;
+	var fha = document.getElementsByName('loan')[1].checked;
 
-// house = houseInfo(100000);
-// console.log(house);
-// console.log(house.fha);
-
+	if (trad || (!fha && !trad)){
+		tradDownPayment(price);
+	}
+	if (fha){
+		fhaDownPayment(price);
+	}
+}
 
 var tradDownPayment = function (price) {
 	var downPayment = Math.round(price * 0.2 * 100) / 100;
@@ -24,8 +20,4 @@ var fhaDownPayment = function (price) {
 	var downPayment = Math.round(price * 0.035 * 100) / 100;
 	document.getElementsByName('downPayFha')[0].value = downPayment;
 	return downPayment;
-}
-
-var roundToCents = function(amount){
-
 }
