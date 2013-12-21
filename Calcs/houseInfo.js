0693b5a0bd5d02aed02e3c6c61054832531cd94c
jtr847 <jtr847@gmail.com>
2013-12-21 01:31:06 -0500

    Down Payments
    Added down payment text fields. Both 20% and 3.5% FHA.

--- Calcs/houseInfo.js
+++ Calcs/houseInfo.js
@@ -1,16 +1,31 @@
 var houseInfo = function (price)
 {
-var houseProps = {cost: price, fha:{}}
-houseProps.downPaymentTrad = price * 0.2;
-houseProps.downPaymentFha = price * 0.035;
-houseProps.fha.downPayment = price * 0.035;
-
-var doubled = price * 2;
-console.log(price)
-console.log(doubled)
-return doubled;
+// var houseProps = {cost: price, fha:{}}
+// houseProps.downPaymentTrad = price * 0.2;
+// // houseProps.downPaymentFha = price * 0.035;
+// houseProps.fha.downPayment = price * 0.035;
+tradDownPayment(price)
+fhaDownPayment(price)
+// return houseProps.downPaymentTrad;
 };
 
 // house = houseInfo(100000);
 // console.log(house);
 // console.log(house.fha);
+
+
+var tradDownPayment = function (price) {
+	var downPayment = Math.round(price * 0.2 * 100) / 100;
+	document.getElementsByName('downPayTrad')[0].value = downPayment;
+	return downPayment;
+}
+
+var fhaDownPayment = function (price) {
+	var downPayment = Math.round(price * 0.035 * 100) / 100;
+	document.getElementsByName('downPayFha')[0].value = downPayment;
+	return downPayment;
+}
+
+var roundToCents = function(amount){
+
+}
