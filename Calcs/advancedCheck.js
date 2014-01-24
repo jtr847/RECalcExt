c02872cc69a763b6d58bb54fabe9bf99be5c7716
Jason Ryan <jtr847@gmail.com>
2014-01-23 22:40:54 -0500

    Styling
    Continued to tweak the style. Made progress in sizing for the extension.

--- Calcs/advancedCheck.js
+++ Calcs/advancedCheck.js
@@ -2,10 +2,11 @@ $(document).ready(function(){
 	$('#advancedChk').click(function(){
 		var advanced = document.getElementsByClassName('advanced');	
 		if (document.getElementById('advancedChk').checked) { 
-			var str = 'none';
+			var str = 'table-row';
 		} else {
-			var str = 'block';
+			var str = 'none';
 		}
+
 		for (var i = 0; i < advanced.length; i++) {
 			advanced[i].style.display = str;
 		}
